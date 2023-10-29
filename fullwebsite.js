bf64f3d2737d4feeef17670c317ddb2513836231
Vednath08 <vedanth.jakkula@outlook.com>
2023-10-29 17:21:34 +0530

    implemented few hovering actions to the web selfpromotion container

--- fullwebsite.js
+++ fullwebsite.js
@@ -4,6 +4,9 @@ var about_horizontal_lineElement = document.getElementById("about-horizontal-lin
 
 var abouttextElement = document.getElementById("selfpromotion-abouttext");
 
+var aboutcontainerElement = document.getElementById("aboutfield-container");
+
+
 
 var experience_fieldElement = document.getElementById("experience-field");
 
@@ -11,12 +14,18 @@ var experience_horizontal_lineElement = document.getElementById("experience-hori
 
 var experiencetextElement = document.getElementById("selfpromotion-experiencetext");
 
+var experiencecontainerElement = document.getElementById("experiencefield-container");
+
+
 
 var project_sectionfieldElement = document.getElementById("project-sectionfield");
 
 var projects_horizontal_lineElement = document.getElementById("projects-horizontal-line");
 
 var projectstextElement = document.getElementById("selfpromotion-projectstext");
+
+var projectcontainerElement = document.getElementById("projectfield-container");
+
 
 
 // On mouseover the horizonatal line will be increased
@@ -51,6 +60,7 @@ project_sectionfieldElement.onmouseover = function(){
     let value = 100;
     projects_horizontal_lineElement.style.width = value+"px";
     projectstextElement.style.color = "black";
+
 }
 
 project_sectionfieldElement.onmouseout = function(){
@@ -58,3 +68,46 @@ project_sectionfieldElement.onmouseout = function(){
     projects_horizontal_lineElement.style.width = value+"px";
     projectstextElement.style.color = "#64675f";
 }
+
+
+// 
+
+aboutcontainerElement.onmouseover = function(){
+    let value = 100;
+    about_horizontal_lineElement.style.width = value+"px";
+    abouttextElement.style.color = "black";
+}
+
+aboutcontainerElement.onmouseout = function(){
+    let value = 50;
+    about_horizontal_lineElement.style.width = value+"px";
+    abouttextElement.style.color = "#64675f";
+}
+
+experiencecontainerElement.onmouseover = function(){
+    let value = 100;
+    experience_horizontal_lineElement.style.width = value+"px";
+    experiencetextElement.style.color = "black";
+}
+
+experiencecontainerElement.onmouseout = function(){
+    let value = 50;
+    experience_horizontal_lineElement.style.width = value+"px";
+    experiencetextElement.style.color = "#64675f";
+}
+
+projectcontainerElement.onmouseover = function(){
+    let value = 100;
+    projects_horizontal_lineElement.style.width = value+"px";
+    projectstextElement.style.color = "black";
+}
+
+projectcontainerElement.onmouseout = function(){
+    let value = 50;
+    projects_horizontal_lineElement.style.width = value+"px";
+    projectstextElement.style.color = "#64675f";
+}
+
+
+
+
